2d3d002d3ed003df2ce326de952cb7030598a2f6
yayaoo3 <geassyayaoo3@gmail.com>
2020-02-17 19:51:11 +0800

    add init eventlog.go

---
+++ api/eventlog.go
@@ -0,0 +1,41 @@
+package api
+
+import (
+	"net/http"
+
+	"github.com/94peter/pica/permission"
+	"github.com/94peter/toad/model"
+)
+
+type LogAPI bool
+
+func (api LogAPI) Enable() bool {
+	return bool(api)
+}
+
+func (api LogAPI) GetAPIs() *[]*APIHandler {
+	return &[]*APIHandler{
+		&APIHandler{Path: "/v1/log", Next: api.getEventLogEndpoint, Method: "GET", Auth: false, Group: permission.All},
+	}
+}
+
+func (api *LogAPI) getEventLogEndpoint(w http.ResponseWriter, req *http.Request) {
+
+	logM := model.GetEventLogModel(di)
+	// queryVar := util.GetQueryValue(req, []string{"branch"}, true)
+	// branch := (*queryVar)["branch"].(string)
+	// if branch == "" {
+	// 	branch = "all"
+	// }
+
+	//data, err := json.Marshal(result)
+	logM.GetEventLogData()
+	data, err := logM.Json("")
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.Write(data)
+}
---
+++ model/eventlog.go
@@ -0,0 +1,117 @@
+package model
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/94peter/toad/resource/db"
+)
+
+type EventLog struct {
+	Account string    `json:"account"`
+	Name    string    `json:"name"`
+	Msg     string    `json:"msg"`
+	Auth    string    `json:"auth"`
+	Type    string    `json:"type"`
+	Date    time.Time `json:"date"`
+}
+
+var (
+	logM *EventLogModel
+)
+
+type EventLogModel struct {
+	imr          interModelRes
+	db           db.InterSQLDB
+	eventLogList []*EventLog
+}
+
+func GetEventLogModel(imr interModelRes) *EventLogModel {
+	if logM != nil {
+		return logM
+	}
+
+	logM = &EventLogModel{
+		imr: imr,
+	}
+	return logM
+}
+
+func (logM *EventLogModel) GetEventLogData() error {
+	const qspl = `SELECT account, name, auth, date, type, msg FROM public.eventlog;`
+	//const qspl = `SELECT arid,sales	FROM public.ar;`
+	db := logM.imr.GetSQLDB()
+	rows, err := db.SQLCommand(fmt.Sprintf(qspl))
+	if err != nil {
+		return nil
+	}
+	var logList []*EventLog
+
+	for rows.Next() {
+		var log EventLog
+
+		// if err := rows.Scan(&r.ARid, &s); err != nil {
+		// 	fmt.Println("err Scan " + err.Error())
+		// }
+		if err := rows.Scan(&log.Account, &log.Name, &log.Auth, &log.Date, &log.Type, &log.Msg); err != nil {
+			fmt.Println("err Scan " + err.Error())
+		}
+
+		logList = append(logList, &log)
+	}
+
+	// out, err := json.Marshal(arList)
+	// if err != nil {
+	// 	panic(err)
+	// }
+	// fmt.Println(string(out))
+	logM.eventLogList = logList
+	return nil
+}
+
+func (logM *EventLogModel) Json(mtype string) ([]byte, error) {
+	switch mtype {
+	case "branch":
+		return json.Marshal(logM.eventLogList)
+	case "account":
+		return json.Marshal(logM.eventLogList)
+	default:
+		fmt.Println("unknown config type")
+		break
+	}
+	return json.Marshal(logM.eventLogList)
+}
+
+func (logM *EventLogModel) CreateEventLog(eventLog *EventLog) (err error) {
+
+	const sql = `INSERT INTO public.EventLog(id, account, name, auth, msg, type, date)	VALUES ($1, $2, $3, $4, $5, $6, $7) ON CONFLICT (id) DO nothing;`
+	//and ( select sum(amount)+$3 FROM public.receipt  where arid = $4 group by arid ) <=  (SELECT amount from public.ar ar WHERE arid = $4);`
+
+	interdb := logM.imr.GetSQLDB()
+	sqldb, err := interdb.ConnectSQLDB()
+	if err != nil {
+		return err
+	}
+	fakeid := time.Now().Unix()
+
+	res, err := sqldb.Exec(sql, fakeid, eventLog.Account, eventLog.Name, eventLog.Auth, eventLog.Msg, eventLog.Type, time.Now())
+	//res, err := sqldb.Exec(sql, unix_time, receivable.Date, receivable.CNo, receivable.Sales)
+	if err != nil {
+		fmt.Println(err)
+		return err
+	}
+	id, err := res.RowsAffected()
+	if err != nil {
+		fmt.Println("PG Affecte Wrong: ", err)
+		return err
+	}
+	fmt.Println(id)
+
+	if id == 0 {
+		return errors.New("Invalid operation, CreateEventLog")
+	}
+
+	return nil
+}

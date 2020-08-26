2d13fd038b0585baa415b46339bd45c2849f2e2a
yayaoo3 <geassyayaoo3@gmail.com>
2020-08-27 00:40:28 +0800

    remove print

--- model/invoice.go
+++ model/invoice.go
@@ -559,14 +559,6 @@ func (invoiceM *InvoiceModel) CreateInvoiceDataFromAPI(iv *Invoice) (*Invoice, e
 	return invoice, nil
 }
 
-// func round(v float64, decimals int) float64 {
-// 	var pow float64 = 1
-// 	for i := 0; i < decimals; i++ {
-// 		pow *= 10
-// 	}
-// 	return float64(int((v*pow)+0.5)) / pow
-// }
-
 func round(x float64, decimals int) float64 {
 	return float64(int(math.Floor(x + 0.5000000001)))
 }
